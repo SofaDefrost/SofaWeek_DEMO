46f6832e29528c348955fa2b320cc1eff1ca20bc
Damien Marchal <damien.marchal@univ-lille1.fr>
2019-11-18 22:19:10 +0100

    Update prefab.

--- repeat.py
+++ repeat.py
@@ -4,7 +4,7 @@ import Sofa.Core
 import Sofa.Helper
     
 @Sofa.PrefabBuilder
-def Repeat(prefab, name="Sphere", filename="mesh/sphere.obj", n=5, spacing=2.0):
+def Repeat(prefab, name="Sphere", filename="mesh/sphere.obj", n=5, spacing=2.0, scale=1.0):
     """Create a Sphere."""
     for i in range(0, n):
         c = prefab.addChild("Sphere"+str(n) )
@@ -12,7 +12,7 @@ def Repeat(prefab, name="Sphere", filename="mesh/sphere.obj", n=5, spacing=2.0):
         c.addObject("UniformMass", name="mass")
 
         v = c.addChild("Visual")
-        v.addObject("MeshObjLoader", name="loader", filename="mesh/sphere.obj", scale=1.0, translation=[spacing*i,0,0])
+        v.addObject("MeshObjLoader", name="loader", filename=filename, scale=scale, translation=[spacing*i,0,0])
         v.addObject("OglModel", src="@./loader")
 
     return prefab
