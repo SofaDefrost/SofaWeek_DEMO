d0b74a360f966ab87a02940de5406e09407e962b
Damien Marchal <damien.marchal@univ-lille1.fr>
2019-11-18 22:11:45 +0100

    Move.

--- scene1.py
+++ scene1.py
@@ -2,9 +2,14 @@
 import sys
 import os
 # all Paths
+sys.path.append('prefab')
 # all Modules:
+from repeat.py import *
 
 
 def createScene(root):
     root.addObject('DefaultAnimationLoop', name='defaultAnimationLoop')
     root.addObject('DefaultVisualManagerLoop', name='defaultVisualManagerLoop')
+    ####################### Prefab: Repeat #########################
+    root.addChild(Repeat(name='Repeat', filename='mesh/sphere.obj', n=5, spacing=2.0))
+
